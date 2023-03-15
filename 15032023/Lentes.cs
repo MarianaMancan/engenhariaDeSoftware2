39c0e6a3666760163267ca5fe4b9a65134ca6f26
MarianaMancan <80931170+MarianaMancan@users.noreply.github.com>
2023-03-15 20:56:42 -0300

    Add files via upload

---
+++ 15032023/Lentes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+    public class Lentes
+    {
+    public float grau;
+    public String tipoFoco;
+    public String formato;
+    public bool antiReflexo;
+
+    public void exibeDados()
+    {
+        Console.WriteLine("Grau: " + grau + " Tipo Foco: " + tipoFoco + " Formato: " + formato);
+    }
+
+    }
+
---
+++ 15032023/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Lentes testelente = new Lentes();
+testelente.grau = 1f;
+testelente.tipoFoco = "multi-Focal";
+testelente.formato = "quadrado";
+testelente.antiReflexo = false;
+
+testelente.exibeDados();
